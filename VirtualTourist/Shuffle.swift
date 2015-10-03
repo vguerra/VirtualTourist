fc0ea2ba577d1644f9d461fa6c069e5f60619bcb
Victor Guerra <vguerra@gmail.com>
2015-10-03 12:35:47 +0200

    Removing commented code.

--- VirtualTourist/Shuffle.swift
+++ VirtualTourist/Shuffle.swift
@@ -10,17 +10,6 @@
 
 import Foundation
 
-//extension Array
-//{
-//    /** Randomizes the order of an array's elements. */
-//    mutating func shuffle() {
-//        for _ in 0..<10
-//        {
-//            sortInPlace { (_,_) in arc4random() < arc4random() }
-//        }
-//    }
-//}
-
 extension MutableCollectionType where Self.Index == Int {
     mutating func shuffleInPlace() {
         let c = self.count
